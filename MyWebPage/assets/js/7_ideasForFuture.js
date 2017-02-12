c626c73e629f7ef08089c0b2df3483ee7f495386
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-02-12 01:04:41 -0500

    pre loader is beetter now

---
+++ MyWebPage/assets/js/7_ideasForFuture.js
@@ -0,0 +1,12 @@
+        $(".futureIdeas-list").owlCarousel({
+            items: 1,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: false,
+            autoHeight: true,
+        });
